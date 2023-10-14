38b0db65e17e89c1cae10a4ac4d919afb26064dc
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2023-10-14 10:50:17 +0200

    Better "call hiearchy" config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "gh", " <Nop>", { desc = "Calling hiearchy" })
+vim.keymap.set("n", "ghi", vim.lsp.buf.incoming_calls, { desc = "Incoming calls" })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,22 @@
 return {
   {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+  {
+    "ray-x/navigator.lua",
+    enabled = true,
+    dependencies = {
+      { "ray-x/guihua.lua", run = "cd lua/fzy && make" },
+      { "neovim/nvim-lspconfig" },
+    },
+    config = function()
+      require("navigator").setup()
+    end,
+  },
+  {
     "crusj/hierarchy-tree-go.nvim",
+    enabled = false,
     dependencies = {
       { "neovim/nvim-lspconfig" },
     },
