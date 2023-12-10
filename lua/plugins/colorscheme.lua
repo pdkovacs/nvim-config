5ba6e1387cf9f36422421c97f0a9751dfa74f4f0
Péter Kovács <peter.dunay.kovacs@gmail.com>
2023-12-10 22:00:55 +0100

    Add "dracula" colorscheme
    foo1

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,6 +9,19 @@ return {
   { "rktjmp/lush.nvim" }, -- Not a colorscheme, gives realtime feedback on colorschemes; required by zenbones
   { "mcchrish/zenbones.nvim" },
   { "EdenEast/nightfox.nvim" },
+  { "Mofiqul/dracula.nvim" },
+  -- {
+  --   "maxmx03/dracula.nvim",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     local dracula = require("dracula")
+  --
+  --     dracula.setup()
+  --
+  --     vim.cmd.colorscheme("dracula")
+  --   end,
+  -- },
   -- Configure LazyVim to load <colorscheme>
   {
     "LazyVim/LazyVim",
