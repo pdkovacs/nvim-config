4112ef111a24daeb1bf2ea24ed6ca7ff66dbecf0
Kovács Péter <peter.dunay.kovacs@gmail.com>
2025-01-12 10:59:38 +0100

    Disable inlay hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,6 +13,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = {
+        enabled = false,
+        exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+      },
       servers = { eslint = {} },
       setup = {
         eslint = function()
